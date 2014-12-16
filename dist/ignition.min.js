eaaac357468355c8e1aace4ad74c176f2437362d
Mac Heller-Ogden <mac@machellerogden.com>
2014-12-16 12:20:46 -0600

    missed dist in last build - incrementing version for bower

--- dist/ignition.min.js
+++ dist/ignition.min.js
@@ -1,5 +1,5 @@
 /*!
- * IgnitionJS v2.0.0 <https://github.com/carsdotcom>
+ * IgnitionJS v2.0.2 <https://github.com/carsdotcom>
  * @license Apache 2.0
  * @copyright 2014 Cars.com <http://www.cars.com/>
  * @author Mac Heller-Ogden
